6c30a214a0ce5f3dee28b499e119fe69fbc6cbd0
Mark Osborn <mosborn@costar.com>
2025-08-12 14:04:13 -0700

    Demonstrate

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,15 +5,15 @@ namespace MagickNET_perceptualhash_change;
 [TestClass]
 public class UnitTest1
 {
-	private const string Version_14_0_0_Hash = "a6784839b28a7658adaa622778d3d6624f1a231e81a7a85cee85a418bb0a869a28bc78a7212838eb8a3628a9fe621de8c68c622fea63ab83ed98327c831b1863f585127884beab934846ff870ec873428e55f896d861c01af401850b48a35f8a5dc621128ce386242d";
-    private const string Version_14_6_0_Hash = "a6783839b28a7658adad622778d3dc624f4a231e81a7a85cee85a418bb0a869a28bc78a720f838eb8a35e8a9fb621de8c689622fda63ab83ed98327c831b1863f585127884beab932846fe870ec873428e55e896d761c01af401850b48a35f8a5dc621128ce386242d";
-    private const string Version_14_7_0_Hash = "a6783839b28a7658adad62ee08d3dc624f4a231e81a7a85cee85a418bb0a869a28bc78a720f838eb8a35e8a9fb621de8c68962ee0a63ab83ed98327c831b1863f585127884beab932846fe870ec873428e55e896d761c01af401850b48a35f8a5dc621128ce386242d";
+	private const string Version_14_0_0_Hash = "af04685a6188478888fe61b5c8b63f61c8ca231e81a7a85cee85a418bb0a869a28bc78aeb3d84e358992e89dfe61f5c8c54162712ceff982a4f81b62819f7834b882f1f85c7cadc9c8497d888f388db061c1d8b29661fb5ae97684fe988e82890f561ce28b8ed6205e";
+    private const string Version_14_6_0_Hash = "af04685a6188478888fe61b5c8b63f61c8ca231e81a7a85cee85a418bb0a869a28bc78aeb3d84e358992e89dfe61f5c8c54162712ceff982a4f81b62819f7834b882f1f85c7cadc9c8497d888f388db061c1d8b29661fb5ae97684fe988e82890f561ce28b8ed6205e";
+    private const string Version_14_7_0_Hash = "af04685a6188478888fe61b5c8b63f61c8ca231e81a7a85cee85a418bb0a869a28bc78aeb3d84e358992e89dfe61f5c8c54162ee0ceff982a4f81b62819f7834b882f1f85c7cadc9c8497d888f388db061c1d8b29662ee0ae97684fe988e82890f561ce28b8ed6205e";
 
     [TestMethod]
 	public void GenerateHash_MagickNET_Q16_AnyCPU_14_0_0()
 	{
 		using var image = new MagickImage("pexels-photo-5563472.jpg");
-		var result = image.PerceptualHash()!.ToString();
+		var result = image.PerceptualHash(ColorSpace.sRGB, ColorSpace.HCLp)!.ToString();
 		Assert.AreEqual(Version_14_0_0_Hash, result);
 	}
 
@@ -21,7 +21,7 @@ public class UnitTest1
     public void GenerateHash_MagickNET_Q16_AnyCPU_14_6_0()
     {
         using var image = new MagickImage("pexels-photo-5563472.jpg");
-        var result = image.PerceptualHash()!.ToString();
+        var result = image.PerceptualHash(ColorSpace.sRGB, ColorSpace.HCLp)!.ToString();
         Assert.AreEqual(Version_14_6_0_Hash, result);
     }
 
@@ -29,23 +29,23 @@ public class UnitTest1
     public void GenerateHash_MagickNET_Q16_AnyCPU_14_7_0()
     {
         using var image = new MagickImage("pexels-photo-5563472.jpg");
-        var result = image.PerceptualHash()!.ToString();
+        var result = image.PerceptualHash(ColorSpace.sRGB, ColorSpace.HCLp)!.ToString();
         Assert.AreEqual(Version_14_7_0_Hash, result);
     }
 
     [TestMethod]
     public void HashDifference_MagickNET_Q16_AnyCPU_14_0_0_VS_MagickNET_Q16_AnyCPU_14_6_0()
     {
-        var hash1 = new PerceptualHash(Version_14_0_0_Hash);
-        var hash2 = new PerceptualHash(Version_14_6_0_Hash);
-        Assert.IsTrue(hash1.SumSquaredDistance(hash2) < 0.0001);
+        var hash1 = new PerceptualHash(Version_14_0_0_Hash, ColorSpace.sRGB, ColorSpace.HCLp);
+        var hash2 = new PerceptualHash(Version_14_6_0_Hash, ColorSpace.sRGB, ColorSpace.HCLp);
+        Assert.IsTrue(hash1.SumSquaredDistance(hash2) == 0);
     }
 
     [TestMethod]
     public void HashDifference_MagickNET_Q16_AnyCPU_14_6_0_VS_MagickNET_Q16_AnyCPU_14_7_0()
     {
-        var hash1 = new PerceptualHash(Version_14_6_0_Hash);
-        var hash2 = new PerceptualHash(Version_14_7_0_Hash);
+        var hash1 = new PerceptualHash(Version_14_6_0_Hash, ColorSpace.sRGB, ColorSpace.HCLp);
+        var hash2 = new PerceptualHash(Version_14_7_0_Hash, ColorSpace.sRGB, ColorSpace.HCLp);
         Assert.IsTrue(hash1.SumSquaredDistance(hash2) < 0.0001);
     }
 }
